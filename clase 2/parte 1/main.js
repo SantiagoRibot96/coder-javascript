e74973e2dab39b65b97d307376d47c8534571b74
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2023-07-22 14:23:57 -0300

    condicionales parte 1

--- clase 2/parte 1/main.js
+++ clase 2/parte 1/main.js
@@ -1,61 +1,101 @@
-//comentario de linea
-
 /*
-comentario
-de
-bloque
+Estructura del if
+
+if(condicion){
+    codigo a ejecutar si es verdadero;
+}
+else{
+
+}
 */
 
-/*declaracion de variables*/
-
-let edad = 15;//creo la variable edad con el numero 15 - es un int
-
-/*declaracion de constantes*/
-
-const numero = 10;
-
-/*utilizar camelCase para declarar variables.*/
-
-let numeroUno = 3;
-let numeroDos = 5;
-
-let resultadoSuma = numeroUno + numeroDos;
-let resultadoResta = numeroUno - numeroDos;
-let resultadoMultiplicacion = numeroUno * numeroDos;
-let resultadoDivision = numeroUno / numeroDos;
-
-console.log("resultado suma:", resultadoSuma); //Muestra por consola lo que pongas entre parentesis.
-console.log("resultado resta:", resultadoResta);
-console.log("resultado multiplicacion:", resultadoMultiplicacion);
-console.log("resultado division:", resultadoDivision);
-
-let saludo = "hola";//es un string por eso las comillas
-let nombre = "andres";
-
-let saludoCompleto = saludo + " " + nombre;
-
-console.log(saludoCompleto);
-
-let nombreIngresado = prompt("Ingrese su nombre");//abre un cuadro de dialogo y lo guarda en la variable. Todo lo que se ingresa por prompt es un string
-alert(nombreIngresado);//muestra un cuadro de dialogo que muestra lo que le ponen
-
-console.log(typeof(numeroUno));
-console.log(typeof(saludo));
+// if(true){
+//     console.log("no soy caniche pero toy");
+// }
+// else{
+//     console.log("nope");
+// }
 
 /*
-parseInt    => de string a int
-parseFloat  => de string a float
-Number      => de string a number
+Todos los numeros a excepcion del 0 son true. Todos los string a excepcion de uno vacio son true
 */
 
-let numeroIngresadoUno = prompt("Ingrese un numero");
-let numeroIngresadoDos = prompt("Ingrese otro numero");
+// if(""){
+//     console.log("toy");
+// }
 
-alert(numeroIngresadoUno + numeroIngresadoDos);
-let numeroIngresadoUnoCast = Number(numeroIngresadoUno);
-let numeroIngresadoDosCast = Number(numeroIngresadoDos);
-alert(numeroIngresadoUnoCast + numeroIngresadoDosCast);
+/*
+= asignacion
+== comparacion
+=== igualdad estricta (tanto valor como tipo)
+*/
 
-/*Puedo hacer el casteo de una*/
+/*
+"anDReS".toLowerCase();
+*/
 
-let numeroCasteado = Number(prompt("Ingrese un numero"));
+// let nombre = prompt("ingrese su nombre");
+
+// if(nombre.toLowerCase() === "miguel")
+// {
+//     alert("bienvenido miguel");
+// }
+// else{
+//     alert("usuario desconocido")
+// }
+
+
+/*
+Estructura del if
+
+if(condicion){
+    codigo a ejecutar si es verdadero;
+}
+else if{
+
+}
+else{
+
+}
+*/
+
+// let edad = parseInt(prompt("Ingrese edad"));
+
+// if(edad >= 18){
+//     alert("Puede ingresar");
+// }
+// else if(edad >= 14){
+//     alert("Puede ingresar con un mayor");
+// }
+// else{
+//     alert("No puede ingresar");
+// }
+
+/*
+isNaN() -> indica si lo que pones dentro es o no es un numero (is Not a Number)
+*/
+
+/*
+Operadores:
+
+AND &&
+OR ||
+not !
+*/
+
+/*
+("hola " + variable) es igual a escribir (`hola ${variable}`) (altgr + cierre de llave)
+*/
+
+// let nombre = prompt("ingese su nombre");
+// let apellido = prompt("ingrese su apellido");
+
+// if(nombre != "" && apellido != "")
+// {
+//     alert(`hola ${nombre} ${apellido}`);
+// }
+// else
+// {
+//     alert("se necesita ingresar nombre y apellido");
+// }
+
