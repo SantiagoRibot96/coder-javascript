7837308bf5b01834a71c25525a0ffb0c186335fa
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2023-07-29 11:35:21 -0300

    clase 3

---
+++ clase 3/parte 1/main.js
@@ -0,0 +1,101 @@
+/*
+Estructura del if
+
+if(condicion){
+    codigo a ejecutar si es verdadero;
+}
+else{
+
+}
+*/
+
+// if(true){
+//     console.log("no soy caniche pero toy");
+// }
+// else{
+//     console.log("nope");
+// }
+
+/*
+Todos los numeros a excepcion del 0 son true. Todos los string a excepcion de uno vacio son true
+*/
+
+// if(""){
+//     console.log("toy");
+// }
+
+/*
+= asignacion
+== comparacion
+=== igualdad estricta (tanto valor como tipo)
+*/
+
+/*
+"anDReS".toLowerCase();
+*/
+
+// let nombre = prompt("ingrese su nombre");
+
+// if(nombre.toLowerCase() === "miguel")
+// {
+//     alert("bienvenido miguel");
+// }
+// else{
+//     alert("usuario desconocido")
+// }
+
+
+/*
+Estructura del if
+
+if(condicion){
+    codigo a ejecutar si es verdadero;
+}
+else if{
+
+}
+else{
+
+}
+*/
+
+// let edad = parseInt(prompt("Ingrese edad"));
+
+// if(edad >= 18){
+//     alert("Puede ingresar");
+// }
+// else if(edad >= 14){
+//     alert("Puede ingresar con un mayor");
+// }
+// else{
+//     alert("No puede ingresar");
+// }
+
+/*
+isNaN() -> indica si lo que pones dentro es o no es un numero (is Not a Number)
+*/
+
+/*
+Operadores:
+
+AND &&
+OR ||
+not !
+*/
+
+/*
+("hola " + variable) es igual a escribir (`hola ${variable}`) (altgr + cierre de llave)
+*/
+
+// let nombre = prompt("ingese su nombre");
+// let apellido = prompt("ingrese su apellido");
+
+// if(nombre != "" && apellido != "")
+// {
+//     alert(`hola ${nombre} ${apellido}`);
+// }
+// else
+// {
+//     alert("se necesita ingresar nombre y apellido");
+// }
+
---
+++ clase 3/parte 2/main.js
@@ -0,0 +1,110 @@
+/*
+Ciclos e iteraciones
+
+for(desde; hasta; actualizacion){
+    codigo a repetir
+}
+*/
+
+// for(let i = 1; i < 10; i++){
+//     console.log(`hola ${i}`);
+// }
+
+
+// let numero = parseInt(prompt("ingrese un numero"));
+
+// for(let i = 1; i < 10; i++){
+//     alert(numero * i);
+// }
+
+/*
+break -> rompe el bucle, lo termina.
+*/
+
+// for(let i = 1; i < 10; i++){
+
+//     if(i == 5){
+//         break;
+//     }
+
+//     console.log(i);
+// }
+
+/*
+continue -> no quiebra el bucle, simplemente se saltea esa vuelta
+*/
+
+// for(let i = 1; i < 10; i++){
+
+//     if(i == 5){
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+/* 
+while(condicion){
+    codigo a ejecutar cuando la condicion se cumple;
+}
+*/
+
+// let usuario = prompt("ingrese su nombre");
+
+// while(usuario != "pepito"){
+//     alert("usuario incorrecto");
+//     usuario = prompt("ingrese su nombre")
+// }
+
+// alert(`Bienvenido ${usuario}`);
+
+/* 
+do{
+    codigo a ejecutar;
+}while(condicion);
+*/
+
+// do{
+//     console.log("hola");
+// }while(false);
+
+/* 
+switch(variable){
+    case valor1:
+        codigo;
+        break;
+
+    case valor2:
+        codigo;
+        break;
+
+    .
+    .
+    .
+
+    default:
+        codigo;
+        break;
+}
+*/
+
+// let moneda = "ars";
+
+// switch(moneda){
+//     case "ars":
+//         console.log("moneda de argentina");
+//         break;
+
+//     case "cop":
+//         console.log("moneda de colombia");
+//         break;
+
+//     case "usd":
+//         console.log("moneda de estados unidos");
+//         break;
+
+//     default:
+//         console.log("moneda no soportada");
+//         break;
+// }
+
