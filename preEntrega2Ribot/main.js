240e49cb9be72da46d2bb859066221dd78e37bcb
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2023-08-14 22:44:01 -0300

    funciones varias, programa andando

--- preEntrega2Ribot/main.js
+++ preEntrega2Ribot/main.js
@@ -68,17 +68,17 @@ function buscarJugador(){
 function ordenar(){
     jugadores.sort(function (a, b) {
         if (a.equipo < b.equipo) {
-            return 1;
+            return -1;
         }
         if (a.equipo > b.equipo) {
-            return -1;
+            return 1;
         }
         
         return 0;
     });
 }
 
-function mostrar(){
+function mostrarJugadores(){
     for (const item of jugadores) {
         let mensaje = `
         Nombre: ${item.nombre}
@@ -89,6 +89,24 @@ function mostrar(){
 
         alert(mensaje);
     }
+}
+
+function mostrarEquipos(){
+    let mensaje = `
+    Equipo A
+    Nombre:     ${jugadores[0].nombre}     ${jugadores[1].nombre}      ${jugadores[2].nombre}      ${jugadores[3].nombre}      ${jugadores[4].nombre}
+    Promedio:   ${promedio("A")}
+    `;
+
+    alert(mensaje);
+
+    mensaje = `
+    Equipo B
+    Nombre:     ${jugadores[5].nombre}     ${jugadores[6].nombre}      ${jugadores[7].nombre}      ${jugadores[8].nombre}      ${jugadores[9].nombre}
+    Promedio:   ${promedio("B")}
+    `;
+
+    alert(mensaje);
 }
 
 function promedio(nombreEquipo){
@@ -102,6 +120,58 @@ function promedio(nombreEquipo){
 
     return(sumaParcial/(jugadores.length/2));
 }
+
+function hallarEquipo(){
+
+    let equipo = 0;
+    let cantidadDEF = 0;
+    let cantidadDEL = 0;
+    let cantidadCEN = 0;
+
+    while(true){
+        for (const item of jugadores) {
+            item.asignar();
+            
+            if(item.equipo === "A"){
+                equipo++;
+    
+                if(item.posicion.toLowerCase() === "del"){
+                    cantidadDEL++;
+                }else if(item.posicion.toLowerCase() === "def"){
+                    cantidadDEF++;
+                }else if(item.posicion.toLowerCase() === "del"){
+                    cantidadCEN++;
+                }
+            }else if(item.equipo === "B"){
+                equipo--;
+    
+                if(item.posicion.toLowerCase() === "del"){
+                    cantidadDEL--;
+                }else if(item.posicion.toLowerCase() === "def"){
+                    cantidadDEF--;
+                }else if(item.posicion.toLowerCase() === "del"){
+                    cantidadCEN--;
+                }
+            }
+        }
+    
+        if(equipo >= -1 && equipo <= 1){//Si ambos equipos tienen la misma cantidad de jugadores o su diferencia es menor a 1 para el caso que sean impares
+    
+            if(promedio("A") - promedio("B") <= 1 && promedio("A") - promedio("B") >= -1){//Si el promedio de ambos equipos es similar (distancia de 1)
+                if((cantidadDEL <= 1 && cantidadDEL >= -1) && (cantidadDEF <= 1 && cantidadDEF >= -1) && (cantidadCEN <= 1 && cantidadCEN >= -1)){
+                    break;
+                }
+            }
+        }
+    
+        equipoA = 0;
+        equipoB = 0;
+        cantidadDEF = 0;
+        cantidadDEL = 0;
+        cantidadCEN = 0;
+    }
+}
+
 
 
 
@@ -119,9 +189,7 @@ Inicio del programa
 
 const jugadores = [];
 
-/* 
-Los siguientes son jugadores precargados para agilizar el proceso de carga. En caso de no necesitarlos solo comentar las lineas.
-*/
+alert("El siguiente programa pide por prompt el nombre de un jugador, su posicion (DEL, CEN, DEF) y el promedio (del 1 al 10).\n\nEn caso de poner FIN, la carga de jugadores termina. Se pueden añadir hasta 10 jugadores");
 
 jugadores.push(new Jugador("Ronaldo", "DEL", 9));
 jugadores.push(new Jugador("Riquelme", "CEN", 8));
@@ -133,7 +201,12 @@ jugadores.push(new Jugador("Pepe", "DEL", 4));
 jugadores.push(new Jugador("Carlos", "DEF", 5));
 jugadores.push(new Jugador("Juan", "CEN", 2));
 
-alert("El siguiente programa pide por prompt el nombre de un jugador, su posicion (DEL, CEN, DEF) y el promedio (del 1 al 10).\n\nEn caso de poner FIN, la carga de jugadores termina. Se pueden añadir hasta 10 jugadores");
+if(!(prompt(`Desea utilizar los ${jugadores.length} precargados? S/N`).toLowerCase() === "s")){
+
+    while(jugadores.length > 0){
+        jugadores.pop();
+    }
+}
 
 let nombreIngresado = prompt("Ingrese el nombre del jugador");
 let posicionIngresado;
@@ -164,53 +237,7 @@ while(nombreIngresado.toLowerCase() != "fin"){
     }
 }
 
-let equipo = 0;
-let cantidadDEF = 0;
-let cantidadDEL = 0;
-let cantidadCEN = 0;
-
-while(true){
-    for (const item of jugadores) {
-        item.asignar();
-        
-        if(item.equipo === "A"){
-            equipo++;
-
-            if(item.posicion.toLowerCase() === "del"){
-                cantidadDEL++;
-            }else if(item.posicion.toLowerCase() === "def"){
-                cantidadDEF++;
-            }else if(item.posicion.toLowerCase() === "del"){
-                cantidadCEN++;
-            }
-        }else if(item.equipo === "B"){
-            equipo--;
-
-            if(item.posicion.toLowerCase() === "del"){
-                cantidadDEL--;
-            }else if(item.posicion.toLowerCase() === "def"){
-                cantidadDEF--;
-            }else if(item.posicion.toLowerCase() === "del"){
-                cantidadCEN--;
-            }
-        }
-    }
-
-    if(equipo >= -1 && equipo <= 1){//Si ambos equipos tienen la misma cantidad de jugadores o su diferencia es menor a 1 para el caso que sean impares
-
-        if(promedio("A") - promedio("B") <= 1 && promedio("A") - promedio("B") >= -1){//Si el promedio de ambos equipos es similar (distancia de 1)
-            if((cantidadDEL <= 1 && cantidadDEL >= -1) && (cantidadDEF <= 1 && cantidadDEF >= -1) && (cantidadCEN <= 1 && cantidadCEN >= -1)){
-                break;
-            }
-        }
-    }
-
-    equipoA = 0;
-    equipoB = 0;
-    cantidadDEF = 0;
-    cantidadDEL = 0;
-    cantidadCEN = 0;
-}
-
+hallarEquipo();
 ordenar();
-mostrar();
+mostrarJugadores();
+mostrarEquipos();
